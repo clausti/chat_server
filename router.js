d9ffd3343686972ef966d4897617ad08ff8f19d5
clausti and guillewu <clausti@gmail.com>
2013-11-04 13:54:01 -0800

    Successfully routing to index and rendering 404

--- router.js
+++ router.js
@@ -4,15 +4,19 @@ var path = require('path');
 var mime = require('mime');
 
 
-function servePage(pageFile, res) {
-	console.log(pageFile);
+var servePage = function(pageFile, res) {
+	console.log('pagefile: ' + pageFile);
+
 	fs.readFile(pageFile, { encoding: 'utf8' }, function(err, data) {
 		if (err) {
-			console.log('you have an error!')
+			console.log("error");
+			res.writeHead(404);
+			res.write('404 not found')
+			res.end();
+		} else {
+			res.write(data);
+			res.end();
 		}
-
-		res.write(data);
-		res.end();
 	});
 }
 
@@ -20,12 +24,14 @@ function servePage(pageFile, res) {
 
 function route(req, res) {
 	var url = req.url;
+	console.log(url);
 
 	if (url === '/') {
 		servePage('index.html', res);
-	} else if (url = '/favicon.ico') {
+	} else if (url == '/favicon.ico') {
 		console.log('no icon yet');
 	} else {
+		console.log('here')
 		servePage(url, res);
 	}
 }
