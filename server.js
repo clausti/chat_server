e0452ba52206afd9873a21ca28a04e2bc9e10306
clausti and guillewu <clausti@gmail.com>
2013-11-04 11:14:58 -0800

    Server working

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
+}).listen(8080);
+
+console.log('server running at http://127.0.0.1:8080/');
+
